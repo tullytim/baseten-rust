f47dc0e41e5b477e1439c5c1acd5765267b9d477
Tim Tully <tully_tim@yahoo.com>
2023-10-14 10:44:21 -0700

    latest

--- baseten/src/lib.rs
+++ baseten/src/lib.rs
@@ -1,14 +1,10 @@
-
 use std::{collections::HashMap, error::Error};
-//use reqwest::header::{HeaderMap, HeaderValue, AUTHORIZATION, ACCEPT};
-//use serde_json::{json, to_string_pretty, Value};
-//use reqwest::{Client, Response};
 
 const BASE_URI: &str = "https://app.baseten.co";
-//const BASE_URI: &str = "http://localhost:8000";
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+
+pub struct Baseten {
+    pub api_key: String,
 }
 
  pub async fn call_model_prompt(model_id:&String, prompt:&String, baseten_api_key:&String) -> Result<String, Box<dyn Error>> {
